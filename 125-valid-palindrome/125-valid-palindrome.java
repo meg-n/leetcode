53ccd4448707ba09e1400355c5b664216c97dcb0
Meghna Nair <n.meghna25@gmail.com>
2022-06-05 18:24:42 -0400

    Time: 44 ms (34.43%), Space: 54.9 MB (10.32%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isPalindrome(String s) {
+    public boolean isPalindrome2(String s) {
         StringBuilder sb = new StringBuilder();
         String sClean = s.toLowerCase().replaceAll("[^a-z0-9]", "").replaceAll("\s","");
         System.out.print(sClean);
@@ -7,6 +7,19 @@ class Solution {
                 sb.append(c);
         }
         if(sb.reverse().toString().equals(sClean)) return true;
+        return false;
+    }    
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        String sClean = s.toLowerCase().replaceAll("[^a-z0-9]", "").replaceAll("\s","");
+        Stack<Character> stack = new Stack<>();
+        for(Character c : sClean.toCharArray()){
+            stack.push(c);
+        }
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        if(sb.toString().equals(sClean))return true;
         return false;
     }
 }
