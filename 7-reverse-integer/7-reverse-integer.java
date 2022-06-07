0f1c7baf4134f003c35001fd3dbff0b6f67141e7
Meghna Nair <n.meghna25@gmail.com>
2022-06-07 00:27:57 -0400

    Time: 3 ms (26.84%), Space: 41.2 MB (55.15%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,10 +1,19 @@
 class Solution {
     public int reverse(int x) {
-    String reversed = new StringBuilder().append(Math.abs(x)).reverse().toString();
-    try {
-        return (x < 0) ? Integer.parseInt(reversed) * -1 : Integer.parseInt(reversed);
-    } catch (NumberFormatException e) {
-        return 0;
-    }
+        boolean neg =false;
+        int sum = 0;
+        if(x<0) {
+            neg = true;
+            x = -x;
+        }
+        while(x >0) {
+            int pop = x%10;
+
+            
+            if(sum >  Integer.MAX_VALUE/10 || (sum == Integer.MAX_VALUE/10 && pop > Integer.MAX_VALUE%10  )) return 0;
+            sum = (sum*10) + pop;
+            x /= 10;
+        }
+    return neg? -sum:sum;
     }
 }
