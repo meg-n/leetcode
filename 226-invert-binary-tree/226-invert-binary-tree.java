e8ff22f8a2ee963c18615ad62fa64f1cc59aec37
Meghna Nair <n.meghna25@gmail.com>
2022-06-07 21:14:10 -0400

    Time: 0 ms (100.00%), Space: 39.9 MB (86.17%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -14,15 +14,42 @@
  * }
  */
 class Solution {
-    // O(n)
+    // O(n) - depth first - stack takes up the size n
+//     public TreeNode invertTree(TreeNode root) {
+//         if(root == null) return root;
+//         invertTree(root.left);
+//         invertTree(root.right);
+        
+//         TreeNode temp = root.right;
+//         root.right = root.left;
+//         root.left = temp;
+//         return root;
+//     }
+    // O(n) - breadth first
+    public TreeNode invertTreeBFS(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            TreeNode currentNode = queue.poll();
+            TreeNode temp = currentNode.right;
+            currentNode.right = currentNode.left;
+            currentNode.left = temp;
+            
+            if(currentNode.left != null) queue.add(currentNode.left);
+            if(currentNode.right != null ) queue.add(currentNode.right);
+        }
+        return root;
+    }
     public TreeNode invertTree(TreeNode root) {
-        if(root == null) return root;
+        if(root == null ) return root;
         invertTree(root.left);
         invertTree(root.right);
         
-        TreeNode temp = root.right;
-        root.right = root.left;
-        root.left = temp;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
         return root;
     }
 }
