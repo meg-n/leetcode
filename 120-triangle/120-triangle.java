b553e731db4942e6970717f55a360ca8a40631dc
Meghna Nair <n.meghna25@gmail.com>
2022-06-13 20:08:04 -0400

    Time: 2 ms (93.50%), Space: 43.8 MB (59.28%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
+    public int minimumTotal1(List<List<Integer>> triangle) {
 	int n = triangle.size();
 	int[][] dp = new int[n+1][n+1];
 
@@ -9,4 +9,19 @@ class Solution {
 
 	return dp[0][0];
     }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp= new int[n+1][n+1];
+        
+        for(int level = n-1; level >=0; level--){
+            for(int i =0;i<=level;i++){
+                dp[level][i] = triangle.get(level).get(i) + Math.min(dp[level+1][i], dp[level+1][i+1]);
+                
+            }
+        }
+        return dp[0][0];
+            
+    }
+
+    
 }
