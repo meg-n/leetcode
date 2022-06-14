b3a24f3b03311608ac7e5acbd47882ed790eb946
Meghna Nair <n.meghna25@gmail.com>
2022-06-14 17:34:09 -0400

    Time: 8 ms (55.88%), Space: 44.8 MB (55.31%) - LeetHub

---
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (o1,o2)->o1[0]-o2[0]);
+        
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i-1][1]>intervals[i][0]) return false;
+        }
+        return true;
+    }
+}
