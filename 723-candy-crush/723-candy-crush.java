26faedeadec61d8446e320dd8c43fca1f403f17d
Meghna Nair <n.meghna25@gmail.com>
2022-07-07 17:56:34 -0400

    Time: 8 ms (58.04%), Space: 46.9 MB (59.21%) - LeetHub

--- 723-candy-crush/723-candy-crush.java
+++ 723-candy-crush/723-candy-crush.java
@@ -3,7 +3,7 @@ class Solution {
         boolean incomp = false;
         int R = board.length, C = board[0].length;
         for(int r = 0; r< R; r++){
-            for(int c = 0; c + 2< C ; c++){
+            for(int c = 0; c< C -2; c++){
                 int v = Math.abs(board[r][c]);
                 if(v!=0 && Math.abs(board[r][c+1]) == v && Math.abs(board[r][c+2]) == v){
                     board[r][c] = board[r][c+1] = board[r][c+2] = -v;
@@ -11,7 +11,7 @@ class Solution {
                 }
             }
         }
-        for(int r = 0; r + 2< R; r++){
+        for(int r = 0; r< R -2; r++){
             for(int c = 0; c< C ; c++){
                 int v = Math.abs(board[r][c]);
                 if(v!=0 && Math.abs(board[r+1][c]) == v && Math.abs(board[r+2][c]) == v){
@@ -22,12 +22,18 @@ class Solution {
         }
         
         for (int c = 0; c < C; ++c) {
-            int wr = R - 1;
+            int l = R - 1;
             for (int r = R-1; r >= 0; --r)
-                if (board[r][c] > 0)
-                    board[wr--][c] = board[r][c];
-            while (wr >= 0)
-                board[wr--][c] = 0;
+                if (board[r][c] > 0){
+                    
+                    int temp = board[r][c];
+                    board[r][c] = board[l][c];
+                    board[l][c] = temp;
+                    l--;
+                }
+                   // board[l--][c] = board[r][c];
+            while (l >= 0)
+                board[l--][c] = 0;
         }
         return incomp ? candyCrush(board) : board;
     }
